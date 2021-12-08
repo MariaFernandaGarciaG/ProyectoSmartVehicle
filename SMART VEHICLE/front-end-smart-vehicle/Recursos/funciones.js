d24bcfc17d05b355241911952d1476bc7955a167
Cris-0001 <cristhian0001@hotmail.com>
2021-12-08 17:37:18 -0500

    conexion frontend con backend

--- SMART VEHICLE/front-end-smart-vehicle/Recursos/funciones.js
+++ SMART VEHICLE/front-end-smart-vehicle/Recursos/funciones.js
@@ -1,5 +1,6 @@
 // Example starter JavaScript for disabling form submissions if there are invalid fields
-(function () {
+
+(function() {
     'use strict'
 
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
@@ -7,20 +8,50 @@
 
     // Loop over them and prevent submission
     Array.prototype.slice.call(forms)
-         .forEach(function (form) {
-             form.addEventListener('submit', function (event) {
-               if (!form.checkValidity()) {
+        .forEach(function(form) {
+            form.addEventListener('submit', function(event) {
+                if (!form.checkValidity()) {
                     event.preventDefault()
                     event.stopPropagation()
-                }else{
+                } else {
                     RegistrarAsesor();
+                    event.preventDefault()
+
                 }
 
                 form.classList.add('was-validated')
             }, false)
-         })
+        })
 })()
 
-function RegistrarAsesor(){
-    alert("Registro Asesor")
+
+function RegistrarAsesor() {
+    let identificacion = document.querySelector("#txtIdentificacion").value;
+    let nombres = document.querySelector("#txtNombres").value;
+    let apellidos = document.querySelector("#txtApellidos").value;
+    let telefono = document.querySelector("#txtTelefono").value;
+    let direccion = document.querySelector("#txtDireccion").value;
+    let email = document.querySelector("#txtEmail").value;
+    
+
+    let url = `http://localhost:3000/asesors`;
+    let datos = {
+        identificacion: identificacion,
+        nombre: nombres,
+        apellido: apellidos,
+        telefono: telefono,
+        direccion: direccion,
+        email: email
+    };
+
+    fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(datos),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(res => res.json())
+        .then(mensaje => {
+            console.log(mensaje)
+        })
 }
