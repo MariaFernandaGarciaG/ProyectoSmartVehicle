0317de4ef6ea2021490a1d44f92b5991094e875e
Cris-0001 <cristhian0001@hotmail.com>
2021-12-08 22:29:43 -0500

    enrutamientos y inico pagina hmtl

--- SMART VEHICLE/frond-end-Angular/src/app/modulos/administracion/administracion-routing.module.ts
+++ SMART VEHICLE/frond-end-Angular/src/app/modulos/administracion/administracion-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BuscarAsesorComponent } from './persona/asesor/buscar-asesor/buscar-asesor.component';
+import { CrearAsesorComponent } from './persona/asesor/crear-asesor/crear-asesor.component';
+import { EditarAsesorComponent } from './persona/asesor/editar-asesor/editar-asesor.component';
+import { EliminarAsesorComponent } from './persona/asesor/eliminar-asesor/eliminar-asesor.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'crear-persona',
+    component: CrearAsesorComponent
+  },
+  {
+    path:'editar-persona',
+    component: EditarAsesorComponent
+  },
+  {
+    path:'eliminar-persona',
+    component: EliminarAsesorComponent
+  },
+  {
+    path:'buscar-persona',
+    component: BuscarAsesorComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
