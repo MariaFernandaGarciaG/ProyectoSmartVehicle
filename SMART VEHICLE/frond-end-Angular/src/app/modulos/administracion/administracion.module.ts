ead7f27403f13a374d4e5874da5692c998e7f123
Cris-0001 <cristhian0001@hotmail.com>
2021-12-08 21:01:12 -0500

    crud-login-pedidos

---
+++ SMART VEHICLE/frond-end-Angular/src/app/modulos/administracion/administracion.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdministracionRoutingModule } from './administracion-routing.module';
+import { CrearAsesorComponent } from './persona/asesor/crear-asesor/crear-asesor.component';
+import { EditarAsesorComponent } from './persona/asesor/editar-asesor/editar-asesor.component';
+import { EliminarAsesorComponent } from './persona/asesor/eliminar-asesor/eliminar-asesor.component';
+import { BuscarAsesorComponent } from './persona/asesor/buscar-asesor/buscar-asesor.component';
+import { CrearProductoComponent } from './producto/crear-producto/crear-producto.component';
+import { EditarProductoComponent } from './producto/editar-producto/editar-producto.component';
+import { EliminarProductoComponent } from './producto/eliminar-producto/eliminar-producto.component';
+import { BuscarProductoComponent } from './producto/buscar-producto/buscar-producto.component';
+
+
+@NgModule({
+  declarations: [
+    CrearAsesorComponent,
+    EditarAsesorComponent,
+    EliminarAsesorComponent,
+    BuscarAsesorComponent,
+    CrearProductoComponent,
+    EditarProductoComponent,
+    EliminarProductoComponent,
+    BuscarProductoComponent
+  ],
+  imports: [
+    CommonModule,
+    AdministracionRoutingModule
+  ]
+})
+export class AdministracionModule { }
